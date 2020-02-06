6aeca6256d6a74ac7c88820d179b0387d6ac10fa
MANBEERGIL <60202053+MANBEERGIL@users.noreply.github.com>
2020-02-06 18:37:54 -0500

    Initial Commit

---
+++ w2020day2Stringexamples/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  w2020day2Stringexamples
+//
+//  Created by MANBEER KAUR on 2020-02-06.
+//  Copyright © 2020 MANBEER KAUR. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
