e1c0bc4c59b945c9ffa01b81de8b983e4973976d
MANBEERGIL <manbeergill545@gmail.com>
2020-02-06 21:12:06 -0500

    array

--- w2020day2Stringexamples/main.swift
+++ w2020day2Stringexamples/main.swift
@@ -47,6 +47,181 @@ print(five.isNumber)
 
 let half = "½" as Character
 print(half.isNumber)
+var a:String?
+ a = "Hello"
+if let x = a
+{
+    print(x.lowercased())
+}
+
+else
+{
+    print("a is nil")
+}
+
+print(a!.lowercased())
+for i in 1...10
+{
+    print(i)
+}
+
+for i in stride(from: 1, to: 10, by: 2)  //for show the odd number
+{
+print(i)
+
+}
+
+for i in stride(from: 10, to: 0, by: -1)  // reverse a array
+{
+print(i)
+}
+
+var country = ["India","Canada","pakistan","Sri lanka"]
+
+for c in country
+{
+    print(c)
+}
+print(country[2])
+print(country.count)
+
+
+country.append("Australia")      //to add in array
+print(country)
+
+country += ["c1" ,"c2"]
+print(country)
+
+
+
+var c1 = country[1...2]   //array slice
+
+
+print(c1)
+
+c1[2] = "Hello"
+
+print(c1)
+
+print(country)    // not any change in country
+
+
+var x = Array(repeating: 0, count: 10)
+//var x = Array<int>(repeating: 0, count: 10)
+print(type(of:c1))
+print(type(of:country))
+print(type(of: x))
+
+
+var z = Array<Double>()     //mention thr type of array
+z.append(100.0)          //adding 100.0 in z
+print(z)
+
+
+print(country.capacity)
+country.insert("Test", at: 1)
+print(country)
+
+country.insert("test1", at: 2)
+print(country)
+print(country.capacity)
+
+let cc = [1,2,3]
+print(cc.capacity)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
